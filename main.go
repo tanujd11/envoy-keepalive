3fbbceb89775c904ee6cbaafd3ec6cc3b03124a7
tanujd11 <dwiveditanuj41@gmail.com>
2023-04-02 12:42:05 +0530

    http server
    Signed-off-by: tanujd11 <dwiveditanuj41@gmail.com>

--- main.go
+++ main.go
@@ -1,58 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"net"
-	"os"
+	"net/http"
+	"time"
 )
 
-// main serves as the program entry point
-func main() {
-	// obtain the port and prefix via program arguments
-	port := fmt.Sprintf(":%s", os.Args[1])
-	prefix := os.Args[2]
+func hello(w http.ResponseWriter, req *http.Request) {
+	time.Sleep(400 * time.Second)
+	fmt.Fprintf(w, "hello\n")
+}
 
-	// create a tcp listener on the given port
-	listener, err := net.Listen("tcp", port)
-	if err != nil {
-		fmt.Println("failed to create listener, err:", err)
-		os.Exit(1)
-	}
-	fmt.Printf("listening on %s, prefix: %s\n", listener.Addr(), prefix)
+func headers(w http.ResponseWriter, req *http.Request) {
 
-	// listen for new connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("failed to accept connection, err:", err)
-			continue
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
-
-		// pass an accepted connection to a handler goroutine
-		go handleConnection(conn, prefix)
 	}
 }
 
-// handleConnection handles the lifetime of a connection
-func handleConnection(conn net.Conn, prefix string) {
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-	for {
-		// read client request data
-		bytes, err := reader.ReadBytes(byte('\n'))
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("failed to read data, err:", err)
-			}
-			return
-		}
-		fmt.Printf("request: %s", bytes)
+func main() {
 
-		// prepend prefix and send as response
-		line := fmt.Sprintf("%s %s", prefix, bytes)
-		fmt.Printf("response: %s", line)
-		conn.Write([]byte(line))
-	}
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":6666", nil)
 }
